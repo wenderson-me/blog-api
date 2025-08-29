a0f6f3d524328c661b2837d7fa35415d87ca3ea7
Wenderson <49308695+wenderson-me@users.noreply.github.com>
2025-08-29 17:13:15 +0100

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ const createUser = async (req, res) => {
   try {
     const { name, email, password, avatar, bio, role } = req.body;
     const user = await User.create({ name, email, password, avatar, bio, role });
-    user.password = undefined; // não retorna senha
+    user.password = undefined;
     res.status(201).json({ success: true, user });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
